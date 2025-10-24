fed11e2156b052c4c2d32c72a15a78344654fba1
JamiledeOliveiraFranquilim <jamyfranquilim@gmail.com>
2025-10-24 09:42:13 -0300

    adicionando logout e arrumando o index

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Feed</title>
+</head>
+<header>
+    <nav>
+        <ul>
+            <li a href="cadastrar_php">Criar Conta</a></li>
+            <li a href="login.php">Login</a></li>
+    </ul>
+</nav>
+</header>
+<body>
+    <h1>Bem vindo ao Kamile!</h1>
+</body>
+</html>
